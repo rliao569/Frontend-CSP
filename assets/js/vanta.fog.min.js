cd40c51d6cb5d4496da285cd90d2ee8483ef4539
ak146 <studentgit34321@outlook.com>
2023-10-18 10:11:22 -0700

    trying something new
    It did not work still

---
+++ assets/js/vanta.fog.min.js
@@ -0,0 +1,5 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanta/dist/vanta.waves.min.js"></script>
+<script>
+  VANTA.WAVES('#my-background')
+</script>
